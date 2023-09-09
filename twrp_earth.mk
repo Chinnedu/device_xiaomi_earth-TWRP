9837b1e79a13f50370306d6279a57015c72b7346
infinixs5lite <kelvinchinedu97@gmail.com>
2023-09-09 23:22:44 +0100

    Update and rename omni_earth.mk to twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -1,16 +1,4 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+-product, vendor/omni/config/common.mk)
 
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
