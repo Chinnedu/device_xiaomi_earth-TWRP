9debbccda0c14108c23e30fc6338d1fcc0e7fb25
infinixs5lite <kelvinchinedu97@gmail.com>
2023-09-09 23:24:42 +0100

    Update twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -1,4 +1,29 @@
--product, vendor/omni/config/common.mk)
+#
+# Copyright (C) 2021 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
@@ -8,10 +33,3 @@ PRODUCT_NAME := omni_earth
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22120RN86C
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_earth-user 12 TP1A.220624.014 V14.0.4.0.TCVMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_earth/earth:12/TP1A.220624.014/V14.0.4.0.TCVMIXM:user/release-keys
