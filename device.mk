7cff7964c519876d3b11e0bbfa5ab7232e744431
infinixs5lite <kelvinchinedu97@gmail.com>
2023-09-10 18:51:48 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
